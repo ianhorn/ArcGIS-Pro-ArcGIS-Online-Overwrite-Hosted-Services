1c99fc68b18a5f6b322e97f082152b8e9c773132
Ian Horn <ian.horn@ky.gov>
2026-06-01 08:44:29 -0400

    Create creds_example.py

---
+++ creds_example.py
@@ -0,0 +1,11 @@
+ORG = 'awesome'
+USERNAME = 'd_boss'
+PASSWORD = 'd_secret_word'
+
+DB_USER = "db_user"
+DB_PASSWORD = "db_password"
+INSTANCE = 'instance.awesome.workplace'
+DATABASE = 'awesome_vectors'
+DATABASE_PLATFORM = 'SQL_SERVER'
+
+DB_CONNECTION_NAME = 'awesome_instance_awesome.sde'
